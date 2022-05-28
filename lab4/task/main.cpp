a735bc28f56d51c1ab3175aaf729e6b8ba215833
Zakhar <zaharsehar@gmail.com>
2022-05-28 13:52:29 +0300

    Add report (to do)

--- lab4/task/main.cpp
+++ lab4/task/main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
+#include <fstream>
 #include "bstree.h"
+
 using namespace std;
 
 int main() {
-
+    ifstream fin("file.txt");
+    for (int i = 1; i <= 20; i++) {
+        cout << i << ")" << endl;
+        for (int j = 0; j < 10000; j++) {
+            char * temp;
+            fin >> temp;
+            
+        }
+    }
     return 0;
 }
